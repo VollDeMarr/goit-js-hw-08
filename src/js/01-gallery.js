5f4f56520537e787afb802567856f5e77454bd3e
Volodymyr Bobanych <97664943+VollDeMarr@users.noreply.github.com>
2022-06-08 09:32:45 +0300

    done first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems.map(makeMarkup).join(``);
+
+gallery.addEventListener('click', onClick);
+gallery.insertAdjacentHTML('beforeend', markup);
+
+function makeMarkup({ original, preview, description }) {
+  return `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+     </a>
+`;
+}
+
+function onClick(event) {
+  event.preventDefault();
+  if (event.target.tagName !== `IMG`) return;
+  var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
