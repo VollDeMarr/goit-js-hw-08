aa0c76c87d5bdd2a65537ef44b53d1790a6ed7fa
Volodymyr Bobanych <97664943+VollDeMarr@users.noreply.github.com>
2022-06-08 20:45:41 +0300

    third task done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,32 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector(`.feedback-form`);
-console.log(form);
-form.addEventListener('input', changes);
+const testVar = {};
 
-function changes(event) {
-  for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-  }
-  console.log(event.target.value);
+// console.log(form);
+form.addEventListener('input', throttle(saveMessage, 500));
+form.addEventListener('submit', onSubmit);
+
+function saveMessage(event) {
+  event.preventDefault();
+  console.log();
+  testVar.email = form.email.value;
+  testVar.message = form.message.value;
+  // console.log(testVar)
+  localStorage.getItem('feedback-form-state');
+  localStorage.setItem('feedback-form-state', JSON.stringify(testVar));
 }
+function onSubmit(event) {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  form.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+function autoCheck() {
+  const date = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (!date) return;
+  Object.keys(date).forEach(el => {
+    form.elements[el].value = date[el];
+  });
+}
+autoCheck();
