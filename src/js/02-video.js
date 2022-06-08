9aa3be38f4803832a3aab1d8863f69dddec85c50
Volodymyr Bobanych <97664943+VollDeMarr@users.noreply.github.com>
2022-06-08 12:00:55 +0300

    done second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const KEY_LS = `video-current-time`;
+const player = new Player('vimeo-player');
+
+const parsedData = JSON.parse(localStorage.getItem(KEY_LS));
+const prevTime = parsedData ? parsedData.seconds : 0;
+
+player.setCurrentTime(prevTime);
+function getCurrentTime(data) {
+  const currentTime = data.seconds;
+  localStorage.setItem(KEY_LS, JSON.stringify({ seconds: currentTime }));
+  console.log(`${currentTime}`);
+}
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,12 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector(`.feedback-form`);
+console.log(form);
+form.addEventListener('input', changes);
+
+function changes(event) {
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+  }
+  console.log(event.target.value);
+}
